6945b7d5d35505dd05cf6dd8a9bf505d3061da56
eunice <eunice.dayrit09@gmail.com>
2020-08-18 22:45:07 -0700

    Added developer info

--- src/game.js
+++ src/game.js
@@ -35,57 +35,19 @@ class Game {
     this.index = 0;
     this.imgIdx = 0;
     this.speed = 2000;
-    this.velocity = 50;
     this.newWord = "";
     this.wordsPosArr = [];
     this.numLives = 5;
     this.score = 0;
     this.startInterval;
     this.drawInterval;
+    this.interval;
     this.img = new Image();
     this.img.src = "./public/images/nature2.jpg";
 
     this.dictionary = new Dictionary();
 
-    
     this.start = this.start.bind(this);
-  }
-
-  addWord() {
-    let newWord = this.dictionary.returnWord();
-    console.log(this.wordsPosArr);
-    let x = Math.floor(Math.random() * 900);
-    let y = 0;
-
-    this.wordsPosArr.push({ word: newWord, xAxis: x, yAxis: y });
-  }
-
-  draw() {
-    for (let i = 0; i < this.wordsPosArr.length; i++) {
-      let el = this.wordsPosArr[i];
-      if (el.yAxis < 640){ 
-        this.ctx.beginPath();
-          this.ctx.rect(el.xAxis, el.yAxis - 15, el.word.length*11, 20, 'grey');
-          this.ctx.fillStyle = "#47a4cf";
-          this.ctx.fill();
-        this.ctx.closePath();
-        
-        this.ctx.font = '15px serif';
-        this.ctx.fillStyle = "#000";
-        this.ctx.fillText(el.word, el.xAxis, el.yAxis)
-        el.yAxis++
-      } else if (el.yAxis === 640) {
-        this.numLives--;
-        // debugger
-        console.log(this.numLives)
-        this.lives.innerHTML = this.numLives;
-        el.yAxis = 5000;
-        if (this.numLives === 0){
-          this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-          this.gameOver();
-        }
-      }
-    }
   }
 
   start(e) {
@@ -100,57 +62,139 @@ class Game {
       this.scoreText.hidden = false;
       this.lives.hidden = false;
       this.livesText.hidden = false;
-      
 
-      this.startInterval = setInterval(() => {
-        this.addWord();
-        console.log(this.speed);
-        console.log(this.velocity);
-      }, this.speed)
-  
+      this.startInterval();
+      // this.drawInterval();
+
+      // this.startInterval = setInterval(() => {
+      //   this.addWord();
+      //   // console.log("speed = ", this.speed);
+      // }, this.speed)
   
       this.drawInterval = setInterval(() => {
         this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
-        this.ctx.drawImage(this.img, 0, this.bgHeight, 1920, 1080, 0, 0, 1040, 640);
+        this.ctx.draw (this.img, 0, this.bgHeight, 1920, 1080, 0, 0, 1040, 640);
         this.match();
         this.draw();
-      }, this.velocity)
-  
+      }, 50)
+
       this.lives.innerHTML = this.numLives;
       this.scoreShow.innerHTML = 0;
 
     }
   }
 
+  startInterval() {
+    this.interval = setInterval(() => {
+            this.addWord();
+            // console.log(this.speed);
+          }, this.speed)
+  }
+
+
+  // drawInterval() {
+  //   setInterval(() => {
+  //           this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+  //           this.ctx.drawImage(this.img, 0, this.bgHeight, 1920, 1080, 0, 0, 1040, 640);
+  //           this.match();
+  //     if (this.numLives === 0) {
+  //       this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  //       this.gameOver();
+  //     }
+  //           this.draw();
+  //         }, 50)
+  //         // }, this.velocity)
+  // }
+
+  addWord() {
+    let newWord = this.dictionary.returnWord();
+    // console.log(this.wordsPosArr);
+    let x = Math.floor(Math.random() * 900);
+    let y = 0;
+
+    this.wordsPosArr.push({ word: newWord, xAxis: x, yAxis: y });
+  }
 
   match() {
     let word = this.wordInput.value.toUpperCase();
     let wordsArr = _.map(this.wordsPosArr, 'word')
     
-    if (wordsArr.includes(word)){
+    if (wordsArr.includes(word)) {
       this.index = wordsArr.indexOf(word);
       this.wordsPosArr.splice(this.index, 1);
 
-      if (this.bgHeight - 20 < 0) {
+      this.wordInput.value = '';
+      this.score++;
+      this.scoreShow.innerHTML = this.score;
+
+      if (this.bgHeight - 100 < 0) {
         this.randomImg();
       } else {
         this.bgHeight = this.bgHeight - 100;
       }
-      
-      this.wordInput.value = '';
-      this.score++;
-      this.scoreShow.innerHTML = this.score;
+
     }
   }
 
+  draw() {
+    for (let i = 0; i < this.wordsPosArr.length; i++) {
+      let el = this.wordsPosArr[i];
+      if (el.yAxis < 640) {
+        this.ctx.beginPath();
+        this.ctx.rect(el.xAxis, el.yAxis - 15, el.word.length * 11, 20, 'grey');
+        this.ctx.fillStyle = "#47a4cf";
+        this.ctx.fill();
+        this.ctx.closePath();
+
+        this.ctx.font = '15px serif';
+        this.ctx.fillStyle = "#000";
+        this.ctx.fillText(el.word, el.xAxis, el.yAxis)
+        el.yAxis++
+      } else if (el.yAxis === 640) {
+        this.numLives--;
+        this.lives.innerHTML = this.numLives;
+        el.yAxis = 5000;
+        if (this.numLives === 0) {
+          this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+          this.gameOver();
+        }
+      }
+    }
+  }
+
+  randomImg() {
+    this.bgHeight = 1100;
+    this.imgIdx = Math.floor(Math.random() * 4);
+    this.img.src = IMAGES[this.imgIdx];
+
+    if (this.speed > 500) {
+      this.speed = this.speed - 500;
+      clearInterval(this.startInterval());
+      // clearInterval(this.startInterval);
+      console.log("speed = ", this.speed);
+    } else if (this.speed <= 500 && this.speed  > 100) {
+      this.speed = this.speed - 100;
+      console.log("speed = ", this.speed);
+    } else if (this.speed <=100 && this.speed > 10) {
+      this.speed = this.speed - 10;
+      console.log("speed = ", this.speed);
+    } else if (this.speed <= 10 && this.speed > 1) {
+      this.speed = this.speed - 1;
+      console.log("speed = ", this.speed);
+    }
+
+  }
+
   gameOver() {
+    // clearInterval(this.interval());
+    // clearInterval(this.drawInterval());
+    clearInterval(this.startInterval());
+    clearInterval(this.drawInterval);
     this.scoreShow.hidden = true;
     this.scoreText.hidden = true;
     this.livesText.hidden = true;
     this.lives.hidden = true;
     this.wordInput.hidden = true;
-    clearInterval(this.startInterval);
-    clearInterval(this.drawInterval);
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
     this.numLives = 5;
     this.wordsPosArr = [];
@@ -163,24 +207,6 @@ class Game {
     this.ctx.fillText("Game Over!", this.canvasWidth / 2 - 70, 250);
     this.ctx.fillText("Press enter to start", this.canvasWidth / 2 - 110, 350);
     this.page.addEventListener('keydown', this.start);
-  }
-
-  randomImg() {
-    if (this.speed > 500) {
-      this.speed = this.speed - 500;
-      this.velocity = this.velocity - 5;
-    } else if (this.speed <= 500 && this.speed  > 100) {
-      this.speed = this.speed - 100;
-      this.velocity = this.velocity - 5;
-    } else if (this.speed <=100 && this.speed > 10) {
-      this.speed = this.speed - 10;
-    } else if (this.speed <= 10 && this.speed > 1) {
-      this.speed = this.speed - 1;
-    }
-
-    this.bgHeight = 1100;
-    this.imgIdx = Math.floor(Math.random() * 4);
-    this.img.src = IMAGES[this.imgIdx];
   }
   
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   const canvas = document.getElementById("game-canvas");
   const ctx = canvas.getContext("2d");
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
   const canvasWidth = 1040;
   const canvasHeight = 640;
   const game = new Game(ctx, page, currentWord, wordInput, scoreText, scoreShow, livesText, lives, canvasWidth, canvasHeight);
